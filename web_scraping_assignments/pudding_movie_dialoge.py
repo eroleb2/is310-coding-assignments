ef01068f1cada5fe708011b7a25f42d3ec6cc571
eroleb2 <143041553+eroleb2@users.noreply.github.com>
2024-02-21 18:18:27 -0600

    Add files via upload

--- web_scraping_assignments/pudding_movie_dialoge.py
+++ web_scraping_assignments/pudding_movie_dialoge.py
@@ -1,12 +1,15 @@
 from bs4 import BeautifulSoup
 import requests
 import pandas as pd
-url = pd.read_csv('cleaned_pudding_data.csv')
-url = url[:1000]
-def scrape_script(url):
-    response = requests.get(url)
-    response = response.text
-    html_string = response
-    return html_string
-url['text'] = url['link'].apply(scrape_script)
-url.to_csv('/Users/erolb/OneDrive/pictures/screenshots/Desktop/is310-coding-assignments/pudding_movie_dialoge.csv', index=False)
+import csv
+df = pd.read_csv('cleaned_pudding_data.csv')
+link = df['link']
+with open('pudding_movie_dialogue.csv', mode='w') as file:
+    writer = csv.writer(file)
+    writer.writerow(['Link', 'Script'])
+    for i in link:
+        response = requests.get(i)
+        if response.status_code != 200:
+            continue
+        soup = bs4.beautifulsoup(response.text, 'html.parser')
+        writer.writerow([i, soup.get_text()[:1000]])
